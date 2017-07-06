62f7fef71e14082f80a4f994f3927dbc2abc164e
Hiroki Sato <hsugar.310@gmail.com>
2017-07-07 01:11:47 +0900

    テンプレートを追加

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define repl(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
+#define rep(i,n) repl(i,0,n)
+#define mp(a,b) make_pair((a),(b))
+#define pb(a) push_back((a))
+#define all(x) (x).begin(),(x).end()
+#define uniq(x) sort(all(x)),(x).erase(unique(all(x)),end(x))
+#define fi first
+#define se second
+#define dbg(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+void _dbg(string){cout<<endl;}
+template<class H,class... T> void _dbg(string s,H h,T... t){int l=s.find(',');cout<<s.substr(0,l)<<" = "<<h<<", ";_dbg(s.substr(l+1),t...);}
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+#define INF 1120000000
+#define long long long // for codeforces
+
+int main(){
+
+  return 0;
+}
