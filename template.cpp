7ae15ce8c672652e900e221f55e0d3fbfe3f4319
Hiroki Sato <hsugar.310@gmail.com>
2017-08-29 23:49:17 +0900

    update rep macro

--- template.cpp
+++ template.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define repl(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
-#define rep(i,n) repl(i,0,n)
+#define GET_MACRO(_1, _2, _3, NAME, ...) NAME
+#define _repl(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
+#define _rep(i,n) _repl(i,0,n)
+#define rep(...) GET_MACRO(__VA_ARGS__, _repl, _rep)(__VA_ARGS__)
 #define mp(a,b) make_pair((a),(b))
 #define pb(a) push_back((a))
 #define all(x) (x).begin(),(x).end()
@@ -11,8 +13,8 @@ using namespace std;
 #define dbg(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
 void _dbg(string){cout<<endl;}
 template<class H,class... T> void _dbg(string s,H h,T... t){int l=s.find(',');cout<<s.substr(0,l)<<" = "<<h<<", ";_dbg(s.substr(l+1),t...);}
-template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
-template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+template<class T,class U> ostream& operator<<(ostream &o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream &o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
 
 #define INF 1120000000
 #define long long long // for codeforces
