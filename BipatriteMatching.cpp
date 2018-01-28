e4c4e2d661248123ebbc050e4a88b8e0ca822982
Hiroki Sato <hsugar.310@gmail.com>
2018-01-28 17:15:08 +0900

    Code clean up Bipartite Matching

--- BipatriteMatching.cpp
+++ BipatriteMatching.cpp
@@ -1,42 +1,50 @@
-// 2部グラフのマッチング
-// O(VE)
-class BipartiteMatching{
+// 2部グラフのマッチング O(VE)
+class BipartiteMatching {
 private:
-  int n; //頂点数
-  vector<vector<int> > graph; //グラフ
-  vector<int> match; //マッチングペア
+  int n;
+  vector<vector<int>> graph;
+  vector<int> match;
   vector<bool> used;
   bool dfs(int v){
-    used[v]=true;
-    rep(i, graph[v].size()){
-      int u = graph[v][i], w = match[u];
-      if(w<0 || (!used[w] && dfs(w)) ){ //増加路が存在したら，つなぎかえる
-        match[v]=u;
-        match[u]=v;
+    used[v] = true;
+    for(int u : graph[v]){
+      int w = match[u];
+      if(w<0 || (!used[w] && dfs(w))){
+        match[v] = u;
+        match[u] = v;
         return true;
       }
     }
     return false;
   }
 public:
-  BipartiteMatching(int nn) : n(nn){
+  BipartiteMatching(int _n) : n(_n){
     graph.resize(n);
     match.resize(n);
     used.resize(n);
   }
-  void add_edge(int u, int v){ //0-indexedでマッチング辺の追加
+  void add_edge(int u, int v){ // 0-indexed
     graph[u].pb(v);
     graph[v].pb(u);
   }
-  int matching(){ //マッチング数を返す
-    int res=0;
-    fill(all(match), -1);
+  int matching(){ // matching size
+    int res = 0;
+    fill(begin(match), end(match), -1);
     rep(v, n){
-      if(match[v]<0){
-        fill(all(used), false);
-        if(dfs(v)) res++; // 増加路が存在したので，マッチングが増加
+      if(match[v] < 0){
+        fill(begin(used), end(used), false);
+        if(dfs(v)) res++;
       }
     }
     return res;
   }
-}; //END class BipartiteMatching
+};
+
+/*
+  2部グラフの場合，
+    |最大マッチング| = |最小点カバー|
+  一般に，
+    |最大安定集合| + |最小点カバー| = |V|
+  また，各連結集合に対して，
+    |最大マッチング| + |最小辺カバー| = |V|
+*/
