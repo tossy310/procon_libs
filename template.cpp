7c1f5704e420096a6e20881e7574f536b84a2674
Hiroki Sato <hsugar.310@gmail.com>
2018-02-04 11:58:46 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define GET_MACRO(_1, _2, _3, NAME, ...) NAME
+#define _MACRO(_1, _2, _3, NAME, ...) NAME
 #define _repl(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
 #define _rep(i,n) _repl(i,0,n)
-#define rep(...) GET_MACRO(__VA_ARGS__, _repl, _rep)(__VA_ARGS__)
-#define mp(a,b) make_pair((a),(b))
-#define pb(a) push_back((a))
-#define all(x) (x).begin(),(x).end()
+#define rep(...) _MACRO(__VA_ARGS__, _repl, _rep)(__VA_ARGS__)
+#define mp make_pair
+#define pb push_back
+#define all(x) begin(x),end(x)
 #define uniq(x) sort(all(x)),(x).erase(unique(all(x)),end(x))
 #define fi first
 #define se second
 #define dbg(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
-void _dbg(string){cout<<endl;}
-template<class H,class... T> void _dbg(string s,H h,T... t){int l=s.find(',');cout<<s.substr(0,l)<<" = "<<h<<", ";_dbg(s.substr(l+1),t...);}
+void _dbg(string){cerr<<endl;}
+template<class H,class... T> void _dbg(string s,H h,T... t){int l=s.find(',');cerr<<s.substr(0,l)<<" = "<<h<<", ";_dbg(s.substr(l+1),t...);}
 template<class T,class U> ostream& operator<<(ostream &o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
 template<class T> ostream& operator<<(ostream &o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
 
