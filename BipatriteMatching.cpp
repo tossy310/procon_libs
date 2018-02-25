ad592e1c457bd9a20df31e360c05a3f38d22b0d7
Hiroki Sato <hsugar.310@gmail.com>
2018-02-26 01:09:51 +0900

    Add minEdgeCover for bipartite graph

--- BipatriteMatching.cpp
+++ BipatriteMatching.cpp
@@ -1,4 +1,4 @@
-// 2部グラフのマッチング O(VE)
+// 2部グラフのマッチング O(VE), Dinicより遅め
 class BipartiteMatching {
 private:
   int n;
@@ -48,3 +48,23 @@ public:
   また，各連結集合に対して，
     |最大マッチング| + |最小辺カバー| = |V|
 */
+
+
+// 2部マッチングの片方が[0,left_n)のときの最小頂点被覆
+// Dinicのときは (sから到達不可な左側頂点) ⋃ (sから到達可能な右側頂点) が答えなので，残余グラフをsからdfsで求まる．
+  vector<bool> color;
+  void color_dfs(int v, int left_n){
+    color[v] = true;
+    for(auto to : graph[v]){
+      if((v < left_n)^(match[v]==to)){
+        if(!color[to]) color_dfs(to, left_n);
+      }
+    }
+  }
+  vector<int> minEdgeCover(int left_n){
+    color.resize(n,false);
+    rep(i,left_n) if(match[i]==-1) color_dfs(i, left_n);
+    vector<int> res;
+    rep(i,n) if((i<left_n) ^ color[i]) res.pb(i);
+    return res;
+  }
